cd1bd1df0d25c68a98b1c61341e344d9d190844d
koyachi <rtk2106@gmail.com>
2014-08-06 09:20:56 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# go-atkinson
+
+Atkinson Dithering with Go.
+
+
+# Links
+
+- http://www.tannerhelland.com/4660/dithering-eleven-algorithms-source-code/
---
+++ example/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"../"
+	"fmt"
+	"image/jpeg"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	fmt.Printf("processing...\n")
+	imagePath := "./lena.jpg"
+	//imagePath := "./Portal_Companion_Cube.jpg"
+	img, err := atkinson.Run(imagePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path, err := filepath.Abs("result.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = jpeg.Encode(file, img, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
